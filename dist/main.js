c9143545ce86f2be4897020ae441347def6d83e3
Remco <Remco@maxserv.nl>
2016-07-24 19:22:00 +0200

    Converted the tutorial scripts into a new base

--- dist/main.js
+++ dist/main.js
@@ -1,38 +1,65 @@
-var harvester = require('harvester');
+//Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+//Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Builder1',{ role: 'builder' } );
+//Game.creeps['Harvester1'].suicide()
+//http://support.screeps.com/hc/en-us/articles/205990342-StructureSpawn#renewCreep
+//Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
+
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
+    var tower = Game.getObjectById('42a5c224991644d3c69d69e5');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+    });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgraders.length);
+
+    if(harvesters.length < 2) {
+        var newName = Game.spawns['Bastion'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    if(upgraders.length < 1) {
+        var newName = Game.spawns['Bastion'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+
+    for(var name in Game.rooms) {
+        console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-
         if(creep.memory.role == 'harvester') {
-            harvester(creep);
+            roleHarvester.run(creep);
         }
-
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
         if(creep.memory.role == 'builder') {
-
-            if(creep.carry.energy == 0) {
-                if(Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Game.spawns.Bastion);
-                }
-            }
-            else {
-                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-                if(targets.length) {
-                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0]);
-                    }
-                }
-            }
-        }
-
-        if(creep.memory.role == 'guard') {
-            var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-            if(targets.length) {
-                if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
+            roleBuilder.run(creep);
         }
     }
 }
