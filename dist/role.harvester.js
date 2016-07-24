4b613565892031c8104cf7a13d361aef2bfcfdfe
Remco <Remco@maxserv.nl>
2016-07-24 20:02:43 +0200

    Modified harvesters to start upgrading the controller if they're idle. Optimized the upgrader code to empty energy before running back.

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -1,25 +1,52 @@
 var roleHarvester = {
-
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
+        if(creep.memory.dumping && creep.carry.energy == 0) {
+            creep.memory.dumping = false;
+            creep.memory.target  = false;
+            creep.say('harvesting');
+        }
+        if(!creep.memory.dumping && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.dumping = true;
+            creep.memory.target = false;
+            creep.say('Dumping Energy');
+        }
+        if(creep.memory.dumping) {
+            if(creep.memory.target == false) {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            }
+            });
+                if (targets.length > 0) {
+                    creep.memory.target = targets[0].id;
+                } else {
+                    creep.memory.target = creep.room.controller.id;
+                }
+            }
+            var target = Game.getObjectById(creep.memory.target);
+            if (target == null) {
+                creep.memory.target = false;
+            } else {
+                switch(target.structureType) {
+                    case 'spawn':
+                        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target);
+                        }
+                        break;
+                    case 'controller':
+                        if (creep.upgradeController(target) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target);
+                        }
+                        break;
+                }
+            }
+        } else {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-        }
-        });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
             }
         }
     }
--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -2,15 +2,23 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy == 0) {
+        if(creep.memory.dumping && creep.carry.energy == 0) {
+            creep.memory.dumping = false;
+            creep.say('Harvesting');
+        }
+        if(!creep.memory.dumping && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.dumping = true;
+            creep.say('Upgrading');
+        }
+        if(creep.memory.dumping) {
+            var target = creep.room.controller;
+            if (creep.upgradeController(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        } else {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
             }
         }
     }
