aec1e58215770af787edcc56654aee09b17067e3
Remco <Remco@maxserv.nl>
2016-07-28 00:36:01 +0200

    - Removed nextStepIntoRoom from RemoteBuilder because it's using a PathFinder path now, so probably doesn't need it. I hope. - Reformatted and fixed Inspector warnings.

--- dist/roles/role.remotebuilder.js
+++ dist/roles/role.remotebuilder.js
@@ -10,7 +10,7 @@ function RoleRemoteBuilder() {
     this.creep = false;
     this.max = function (c) {
         return 1;
-    }
+    };
     this.getBody = function (capacity) {
         var numParts = _.floor((capacity - 100) / UtilCreep.calculateRequiredEnergy(this.bodyPart));
         var body = [];
@@ -19,14 +19,14 @@ function RoleRemoteBuilder() {
         }
         return body.concat([WORK]);
     };
-    this.deserializePathFinderPath = function(p) {
+    this.deserializePathFinderPath = function (p) {
         var path = [];
-        _.each(p, function(x) {
+        _.each(p, function (x) {
             path.push(new RoomPosition(x.x, x.y, x.roomName));
         }, this);
         return path;
-    }
-    this.findHomePath = function() {
+    };
+    this.findHomePath = function () {
         //find a suitable container
         var homeRoom = Game.rooms[this.homeFlag.pos.roomName];
         var containers = homeRoom.find(FIND_STRUCTURES, {
@@ -34,18 +34,18 @@ function RoleRemoteBuilder() {
             && s.store[RESOURCE_ENERGY] > this.creep.carryCapacity
         });
         console.log(containers);
-        if(containers.length > 0) {
-            var path = PathFinder.search(this.creep.pos, _.map(containers, function(source) {
+        if (containers.length > 0) {
+            var path = PathFinder.search(this.creep.pos, _.map(containers, function (source) {
                 // We can't actually walk on sources-- set `range` to 1 so we path
                 // next to it.
                 return {pos: source.pos, range: 1};
-            }),     {
+            }), {
                 // We need to set the defaults costs higher so that we
                 // can set the road cost lower in `roomCallback`
                 plainCost: 2,
                 swampCost: 10,
 
-                roomCallback: function(roomName) {
+                roomCallback: function (roomName) {
 
                     let room = Game.rooms[roomName];
                     // In this example `room` will always exist, but since PathFinder
@@ -53,27 +53,26 @@ function RoleRemoteBuilder() {
                     if (!room) return;
                     let costs = new PathFinder.CostMatrix;
 
-                    room.find(FIND_STRUCTURES).forEach(function(structure) {
+                    room.find(FIND_STRUCTURES).forEach(function (structure) {
                         if (structure.structureType === STRUCTURE_ROAD) {
                             // Favor roads over plain tiles
                             costs.set(structure.pos.x, structure.pos.y, 1);
                         } else if (structure.structureType !== STRUCTURE_CONTAINER &&
-                            (structure.structureType !== STRUCTURE_RAMPART ||
-                            !structure.my)) {
+                            (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
                             // Can't walk through non-walkable buildings
                             costs.set(structure.pos.x, structure.pos.y, 0xff);
                         }
                     });
 
                     // Avoid creeps in the room
-                    room.find(FIND_CREEPS).forEach(function(creep) {
+                    room.find(FIND_CREEPS).forEach(function (creep) {
                         costs.set(creep.pos.x, creep.pos.y, 0xff);
                     });
 
                     return costs;
                 },
             });
-            if(path.path.length < 1) {
+            if (path.path.length < 1) {
                 delete this.creep.memory.homePath;
                 delete this.creep.memory.homePathContainer;
             } else {
@@ -83,7 +82,7 @@ function RoleRemoteBuilder() {
                     if (_.last(path.path).isNearTo(c, 1)) {
                         this.creep.memory.homePathContainer = c.id;
                     }
-                });
+                }, this);
             }
         }
     };
@@ -93,28 +92,22 @@ function RoleRemoteBuilder() {
             if (this.creep.room.name != this.targetFlag.pos.roomName) {
                 //first make sure we fill up on energy before embarking on an adventure.
                 if (this.creep.carry.energy != this.creep.carryCapacity) {
-                    //dirty fix to get out of map exit.
-                    if (this.nextStepIntoRoom(this.creep) != false) {
-                        //this.creep.moveTo(this.nextStepIntoRoom());
+                    if (!this.creep.memory.homePath) {
+                        this.harvestFromContainersAndSources();
+                        this.creep.memory.runBack = true;
                     } else {
-                        if(!this.creep.memory.homePath) {
-                            this.harvestFromContainersAndSources();
-                            this.creep.memory.runBack = true;
-                        } else {
-                            var c = Game.getObjectById(this.creep.memory.homePathContainer);
-                            if(!this.creep.pos.isNearTo(c)) {
-                                var path = this.deserializePathFinderPath(this.creep.memory.homePath);
-                                var log = this.creep.moveByPath(path);
-                                if (log == ERR_NOT_FOUND) {
-                                    this.findHomePath();
-                                }
-                            } else {
-                                delete this.creep.memory.homePath;
-                                delete this.creep.memory.homePathContainer;
-                                this.harvestFromContainersAndSources();
+                        var c = Game.getObjectById(this.creep.memory.homePathContainer);
+                        if (!this.creep.pos.isNearTo(c)) {
+                            var path = this.deserializePathFinderPath(this.creep.memory.homePath);
+                            var log = this.creep.moveByPath(path);
+                            if (log == ERR_NOT_FOUND) {
+                                this.findHomePath();
                             }
+                        } else {
+                            delete this.creep.memory.homePath;
+                            delete this.creep.memory.homePathContainer;
+                            this.harvestFromContainersAndSources();
                         }
-
                     }
                 } else {
                     //with full energy, move to the next room.
@@ -151,19 +144,14 @@ function RoleRemoteBuilder() {
                     }
                 } else {
                     //Run back to mommy for more energy.
-                    if(!this.creep.memory.homePath) {
-                       this.findHomePath();
+                    if (!this.creep.memory.homePath) {
+                        this.findHomePath();
                     }
                     var path = this.deserializePathFinderPath(this.creep.memory.homePath);
                     var log = this.creep.moveByPath(path);
                     if (log == ERR_NOT_FOUND) {
                         this.findHomePath();
                     }
-                    /**
-                    var exitDir = Game.map.findExit(this.creep.room.name, this.homeFlag.pos.roomName);
-                    var Exit = this.creep.pos.findClosestByPath(exitDir);
-                    this.creep.moveTo(Exit);
-                     **/
                 }
             }
         }
