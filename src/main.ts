6abfa376f780b3395562b9226f8ca5d3d3e67f93
Remco <Remco@maxserv.nl>
2016-07-30 00:13:11 +0200

    Found a way to include plain JS files into the TS tree. Included screeps-profiler this way.

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@
  * BEFORE CHANGING THIS FILE, make sure you read this:
  * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
  */
-
+import * as Profiler from "./lib/screeps-profiler";
 import * as MemoryManager from "./shared/memoryManager";
 import * as RoomManager from "./components/rooms/roomManager";
 import * as SpawnManager from "./components/spawns/spawnManager";
 import * as SourceManager from "./components/sources/sourceManager";
 import * as CreepManager from "./components/creeps/creepManager";
 
+
+Profiler.enable();
+
 // This code is executed only when Screeps system reloads your script.
 // Use this bootstrap wisely. You can cache some of your stuff to save CPU
 // You should extend prototypes before game loop in here.
-
 RoomManager.loadRooms();
 SpawnManager.loadSpawns();
 SourceManager.loadSources();
@@ -24,8 +26,10 @@ SourceManager.loadSources();
 
 export function loop() {
 	// This is executed every tick
+	Profiler.wrap(function() {
+		MemoryManager.loadMemory();
+		CreepManager.loadCreeps();
+		CreepManager.governCreeps();
+	});
 
-	MemoryManager.loadMemory();
-	CreepManager.loadCreeps();
-	CreepManager.governCreeps();
 }
